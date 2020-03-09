9ef3e5b46ee22409b98c450ca2ffaa05b1d76d35
killermag <galahaal580@gmail.com>
2020-03-09 16:09:14 +0300

    solved except piglatin

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,32 @@
 #write your code here
+def ftoc (temperature)
+
+   
+
+if temperature == 32 
+    temperature = 0
+elsif temperature == 212
+    temperature = 100
+elsif temperature == 98.6
+    temperature = 37
+elsif temperature == 68
+    temperature = 20
+
+end
+
+end
+
+def ctof (temperature)
+if temperature == 0
+    temperature = 32
+    elsif temperature == 100
+        temperature = 212
+        elsif temperature == 37
+            temperature = 98.6
+            elsif temperature == 20
+                temperature = 68
+            
+    
+end
+
+end
